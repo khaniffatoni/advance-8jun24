80d15b4d8d4b185ec86c5e89d2aed3c0871dc4da
gustavsr1994 <gustavr1994@gmail.com>
2024-06-29 11:23:38 +0700

    integrate with firebase message

--- flutter_application_advance/lib/commons/config_notification.dart
+++ flutter_application_advance/lib/commons/config_notification.dart
@@ -6,6 +6,8 @@ import 'package:permission_handler/permission_handler.dart';
 class ConfigNotification {
   AwesomeNotifications awesomeNotifications = AwesomeNotifications();
   void initialNotification() async {
+    var tokenFirebase = await FirebaseMessaging.instance.getToken();
+    print('Token Firebase : $tokenFirebase');
     await awesomeNotifications.initialize(
         null,
         [
@@ -18,14 +20,25 @@ class ConfigNotification {
           )
         ],
         debug: true);
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(alert: true, sound: true);
     FirebaseMessaging.onMessage.listen((event) {
-      showNotification(event.notification!.title ?? '', event.notification!.body ?? '');
+      showNotification(
+          event.notification!.title ?? '', event.notification!.body ?? '');
     });
+    FirebaseMessaging.onBackgroundMessage((message) => showNotification(
+        message.notification!.title ?? '', message.notification!.body ?? ''));
   }
 
-  void showNotification(String title, String body) async {
+  Future<void> showNotification(String title, String body) async {
     await awesomeNotifications.createNotification(
         content: NotificationContent(
-            id: -1, channelKey: 'notif', title: title, body: body));
+            id: -1,
+            channelKey: 'notif',
+            title: title,
+            body: body,
+            notificationLayout: NotificationLayout.BigPicture,
+            bigPicture:
+                'https://www.shutterstock.com/image-vector/88-3d-flash-sale-banner-260nw-2176913847.jpg'));
   }
 }
