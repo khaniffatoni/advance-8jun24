99e3c20216a4b3300bbba8ebb3a2624eb8e611fe
gustavsr1994 <gustavr1994@gmail.com>
2024-06-29 10:55:42 +0700

    add firebase message

--- flutter_application_advance/lib/commons/config_notification.dart
+++ flutter_application_advance/lib/commons/config_notification.dart
@@ -1,4 +1,5 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -17,6 +18,9 @@ class ConfigNotification {
           )
         ],
         debug: true);
+    FirebaseMessaging.onMessage.listen((event) {
+      showNotification(event.notification!.title ?? '', event.notification!.body ?? '');
+    });
   }
 
   void showNotification(String title, String body) async {
